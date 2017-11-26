39a13e28deb4ebaec4f1c1b46ae233c16f127e6a
Sathvik777 <katam.sathvik@gmail.com>
2017-11-26 09:00:29 +0100

    filtering stoping words

--- tokenizign.py
+++ tokenizign.py
@@ -1,2 +1,22 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
+test_sentence = "This a Test proving NLTK stopping words work, a the an my our and, should not be printed"
+
+
+#Setting language to english
+stop_words = set(stopwords.words("english"))
+
+words_from_example_sentnce = word_tokenize(test_sentence)
+
+words_after_filter_stop_words = []
+
+for w in words_from_example_sentnce:
+    if w not in stop_words:
+        words_after_filter_stop_words.append(w)
+
+
+# Fancy one lineer if you like, I do not very much for learning
+#words_after_filter_stop_words = [w for w in words_from_example_sentnce if not w in stop_words]
+
+print(words_after_filter_stop_words)
